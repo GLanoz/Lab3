e0a619ed643fb202681a6ebb59020357f5137de4
Lanoz <vladpisarenko97@gmail.com>
2023-04-06 10:53:09 +0300

    .

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
     <div class="wrapper">
 
-        <div class="header"> <?php echo "<h1> Я вивчаю PHP! </h1>"; ?> </div>
+        <div class="header"> <?php include('sections/header.php') ?> </div>
 
         <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
         <label for="openSidebarMenu" class="sidebarIconToggle">
@@ -28,6 +28,8 @@
                 <li>Владислав Писаренко <span>Я вивчаю PHP!</span></li>
                 <li><a href="https://github.com/GLanoz" target="_blank">GitHub</a></li>
                 <li><a href="https://www.linkedin.com/in/vladyslav-pysarenko-396670220/" target="_blank">Linkedin</a></li>
+                <li><a href="sections/table.php" target="_blank">Таблиця</a> </li>
+                <li><a href="sections/array.php" target="_blank">Масив</a> </li>
             </ul>
         </div>
 
@@ -35,90 +37,19 @@
         <section class="table-item" <h1>Таблиця</h1>
 
             <table border="table-border">
-                <?php
-
-                $tableSize = 10;
-
-                echo "<tr><th>0</th>";
-                for ($index = 1; $index <= $tableSize; $index++) {
-                    echo "<th> $index </th>";
-                }
-                echo " </tr> ";
-
-                for ($index = 1; $index <= $tableSize; $index++) {
-                    echo "<tr> <th> $index </th>";
-                    for ($index2 = 1; $index2 <= $tableSize; $index2++) {
-                        echo "<td>" . ($index * $index2) . "</td>";
-                    }
-                    echo " </tr> ";
-                }
-                ?>
+                <?php include('sections/table.php') ?>
             </table>
 
             <section class="array">
-                <h1>Масив</h1>
-                <?php
-                // Створюємо масив з 10 випадкових чисел в діапазоні від -10 до 10
-                $numbers = array();
-                for ($i = 0; $i < 10; $i++) {
-                    $numbers[] = rand(-10, 10);
-                }
 
-                // Виводимо масив на екран
-                echo "<h2>Масив у вигляді рядка:</h2>";
-                foreach ($numbers as $number) {
-                    echo $number . " ";
-                }
+                <?php include('sections/array.php') ?>
 
-                echo "<h2>Масив у вигляді стовпчика:</h2>";
-                foreach ($numbers as $number) {
-                    echo $number . "<br>";
-                }
-
-                echo "<h2>Масив у вигляді print_r:</h2>";
-                echo "<pre>";
-                print_r($numbers);
-                echo "</pre>";
-
-                $sum = 0;
-                foreach ($numbers as $number) {
-                    $sum += $number;
-                }
-                echo "<h2>Сума всіх елементів масиву: $sum</h2>";
-
-                $count = 0;
-                $product = 1;
-                $positive_numbers_sum = 0;
-                foreach ($numbers as $number) {
-                    if ($number > 0) {
-                        $count++;
-                        $positive_numbers_sum += $number;
-                    }
-                }
-                if ($count > 0) {
-                    $average = $positive_numbers_sum / $count;
-
-                    foreach ($numbers as $number) {
-                        if ($number > $average) {
-                            $product *= $number;
-                        }
-                    }
-
-                    echo "<h2>Середнє арифметичне серед додатних чисел: $average</h2>";
-                    echo "<h2>Добуток всіх чисел, більших за середнє арифметичне: $product</h2>";
-                } else {
-                    echo "<h2>В масиві немає додатних чисел</h2>";
-                }
-                ?>
             </section>
         </section>
 
 </body>
 <footer>
-    <div class="footer-wrapper">
-        <p>&copy; 2023 Vladyslav Pysarenko. All rights reserved.</p>
-        <p>Все виглядає сиро і не гарно, ну я думаю фронт же був і не потрібен, зробив просто для себе)</p>
-    </div>
+    <?php include('sections/footer.php') ?>
 </footer>
 
 </html>
---
+++ sections/footer.php
@@ -0,0 +1,4 @@
+<div class="footer-wrapper">
+    <p>&copy; 2023 Vladyslav Pysarenko. All rights reserved.</p>
+    <p>Все виглядає сиро і не гарно, ну я думаю фронт же був і не потрібен, зробив просто для себе)</p>
+</div>
